8b5413a8cd7bb640e5ba3a5711f46f7613619d8d
Jiwon Hwang <ediblepotato@gmail.com>
2023-05-11 22:06:21 +0900

    2023.05.11

--- src/FirstPackage/P181187.java
+++ src/FirstPackage/P181187.java
@@ -1,35 +1,25 @@
 package FirstPackage;
 
+import java.lang.Math;
 public class P181187 {
 
     public static long solution(int r1, int r2) {
 
         long answer = 0;
 
-        int julpyun = 0;
-        for (int i=r1; i<=r2; i++) julpyun += 1;
+        for (int x=1; x<=r2; x++) {
+            int lowerBound = (int) Math.ceil(Math.sqrt(Math.pow(r1, 2) - Math.pow(x, 2)));
+            int upperBound = (int) Math.floor(Math.sqrt(Math.pow(r2, 2) - Math.pow(x, 2)));
 
-        for (int i=0; i<=r2; i++) {
-            for (int j=0; j<i; j++) {
-                int tmp = i*i + j*j;
-                if (tmp >= r1*r1 && tmp <= r2*r2) answer += 1;
-            }
+            answer += upperBound - lowerBound + 1;
         }
 
-        answer -= julpyun;
-        answer *= 2;
-        answer += julpyun;
-
-        for (int i=0; i<=r2; i++) {
-            if (2*i*i >= r1*r1 && 2*i*i <= r2*r2) answer += 1;
-        }
-
-        answer *= 4;
-
-        return answer;
+        return 4 * answer;
     }
 
     public static void main(String[] args) {
+
         System.out.println(solution(2,3));
+
     }
 }
