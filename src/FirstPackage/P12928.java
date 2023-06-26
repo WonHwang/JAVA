25398273eca5b0529af48ce09dd8c161f10e7eff
Jiwon Hwang <ediblepotato@gmail.com>
2023-06-26 17:26:11 +0900

    2023.06.26

---
+++ src/FirstPackage/P12928.java
@@ -0,0 +1,12 @@
+package FirstPackage;
+
+public class P12928 {
+
+    public static int solution(int n) {
+        int answer = 0;
+        for (int i=1; i<=n; i++) {
+            if (n%i == 0) answer += i;
+        }
+        return answer;
+    }
+}
