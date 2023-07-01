e376a80bca77e8dde06a2f8f8911285195e5f019
Jiwon Hwang <ediblepotato@gmail.com>
2023-07-01 16:03:49 +0900

    2023.07.01

---
+++ src/FirstPackage/P12944.java
@@ -0,0 +1,12 @@
+package FirstPackage;
+
+import java.util.Arrays
+public class P12944 {
+
+    public static double solution(int[] arr) {
+        double answer = 0;
+        for (int i=0; i<arr.length; i++) answer += arr[i];
+        answer /= arr.length;
+        return answer;
+    }
+}
