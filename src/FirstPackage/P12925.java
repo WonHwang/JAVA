a1c11968f33c1699d01d7e0cfda7c1775595261a
Jiwon Hwang <ediblepotato@gmail.com>
2023-07-09 23:02:48 +0900

    2023.07.09

---
+++ src/FirstPackage/P12925.java
@@ -0,0 +1,10 @@
+package FirstPackage;
+
+public class P12925 {
+
+    public static int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
